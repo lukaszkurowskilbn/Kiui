0a1bfc4c416aeb58d0642271f8001fb72fce1099
Kiui <lukaszkurowskilbn@gmail.com>
2014-09-12 14:19:22 +0200

    pierwsza paczka

---
+++ model3/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿
+namespace model3.Models
+{
+    using System;
+    using System.ComponentModel;
+    class Customer : INotifyPropertyChanged
+    {
+        public Customer(String customerName)
+        {
+          
+
+        }
+        private string _Name;
+        public String Name
+        {
+            get
+            {
+                return _Name;
+
+            }
+            set
+            {
+                _Name = value;
+                OnPropertyChenged("Name");
+            }
+
+        }
+
+    }
+}
